76608fa0a6fd80affb0df01f119fad4b239f5126
m-ji97 <tv5035@naver.com>
2024-04-22 11:53:40 +0900

    240422

---
+++ src/views/ListView.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <table border="1">
+      <tr>
+        <th>이름(name)</th>
+        <th>핸드폰(hp)</th>
+        <th>회사(company)</th>
+        <th>관리</th>
+      </tr>
+      <tr>
+        <td>강호동</td>
+        <td>010-5555-5555</td>
+        <td>02-5555-5555</td>
+        <td>
+          <button type="button">삭제하기</button>
+          <a href="">수정폼이동</a>
+        </td>
+      </tr>
+      <tr>
+        <td>박명수</td>
+        <td>010-4444-4444</td>
+        <td>02-4444-4444</td>
+        <td>
+          <button type="button">삭제하기</button>
+          <a href="">수정폼이동</a>
+        </td>
+      </tr>
+      <tr>
+        <td>박명수</td>
+        <td>010-3333-3333</td>
+        <td>02-3333-3333</td>
+        <td>
+          <button type="button">삭제하기</button>
+          <a href="">수정폼이동</a>
+        </td>
+      </tr>
+      <tr>
+        <td>박명수</td>
+        <td>010-2222-2222</td>
+        <td>02-2222-2222</td>
+        <td>
+          <button type="button">삭제하기</button>
+          <a href="">수정폼이동</a>
+        </td>
+      </tr>
+      <tr>
+        <td>정우성</td>
+        <td>010-1111-1111</td>
+        <td>02-1111-1111</td>
+        <td>
+          <button type="button">삭제하기</button>
+          <a href="">수정폼이동</a>
+        </td>
+      </tr>
+    </table>
+  </div>
+    <!-- <label>이름(name)</label></div>
+  <div><label>핸드폰(hp)</label></div> -->
+  
+</template>
+
+<script>
+
+export default {
+    name: "ListView",
+    components: {},
+    data() {
+        return {};
+    },
+    methods: {},
+    created(){}
+};
+</script>
+<style></style>
